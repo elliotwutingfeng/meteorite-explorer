e61241bbcc1758b6c44c311817bcdfe3646a9021
Tingfeng <wu.tingfeng@u.nus.edu>
2021-11-07 00:25:08 +0800

    Fix appbar

--- src/components/TopAppBar.js
+++ src/components/TopAppBar.js
@@ -1,18 +1,32 @@
 import * as React from "react";
 
+import { useScrollTrigger } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+
 export default function TopAppBar() {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
   return (
-    <AppBar position="static" sx={{ alignItems: "center" }}>
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <span role="img" aria-label="comet">
-            ☄️ Meteorite Explorer
-          </span>
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar
+        position="fixed"
+        sx={{ alignItems: "center" }}
+        elevation={trigger ? 9 : 4}
+      >
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <span role="img" aria-label="comet">
+              ☄️ Meteorite Explorer
+            </span>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {/* Extra Toolbar component to push content below floating AppBar */}
+      <Toolbar />
+    </>
   );
 }
